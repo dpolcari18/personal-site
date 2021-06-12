ef950cf6200b6283798a27edcfe87e43414d18eb
David Polcari <dpolcari18@gmail.com>
2021-06-12 11:50:52 -0500

    working on responsive design again

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -6,8 +6,8 @@ import './Greeting.css'
 const Greeting = () => {
     return(
         <div className='page-view vertical'>
-            <h1>Hi, I'm David!</h1>
-            <h1>I'm a full-stack software engineer in Austin, TX!</h1>
+            <h1 className='line-1 anim-typewriter'>Hi, I'm David!</h1>
+            <h1 className='line-1 anim-typewriter-two'>I'm a full-stack software engineer in Austin, TX!</h1>
         </div>
     )
 }
