4b9ff20b0bc0db813dc5d9cc774598c36cd6305e
David Polcari <dpolcari18@gmail.com>
2021-06-12 21:08:53 -0500

    changed text on gretting page

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -5,9 +5,9 @@ import './Greeting.css'
 
 const Greeting = () => {
     return(
-        <div className='page-view vertical' id='greeting-div'>
+        <div className='page-view vertical'>
             <h1 className='line-1 anim-typewriter'>Hi, I'm David!</h1>
-            <h1 className='line-1 anim-typewriter-two'>I'm a full-stack software engineer in Austin, TX!</h1>
+            <h1 className='line-1 anim-typewriter-two'>I'm a full-stack software engineer in Austin, TX.</h1>
         </div>
     )
 }
