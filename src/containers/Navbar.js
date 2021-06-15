e752b97a1c7e1e398fa1b7d849b5d87ec1936744
David Polcari <dpolcari18@gmail.com>
2021-06-15 18:29:49 -0500

    hamburger menu working need to fix layout

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -3,6 +3,10 @@ import React, { useState, useEffect } from 'react'
 // styling
 import './Navbar.css'
 
+// font awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
+
 const Navbar = () => {
 
     //local state
@@ -65,22 +69,56 @@ const Navbar = () => {
             )
         } else {
             return (
-                <nav role='navigation'>
-                    <div id='menu-toggle'>
-                        <input type='checkbox'></input>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <ul id='menu'>
-                            <a href='#home-div'>Home</a>
-                            <a href='#about-div'>About</a>
-                            <a href='#skills-div'>Skills</a>
-                            <a href='#portfolio-div'>Portfolio</a>
-                            <a href='#blog-div'>Blog</a>
-                            <a href='#contact-div'>Contact</a>
+                <header className='sticky'>
+                    <a href="#main-menu"
+                        id="main-menu-toggle"
+                        class="menu-toggle"
+                        aria-label="Open main menu">
+                        <FontAwesomeIcon icon={faBars} />
+                        <span class="sr-only">Open main menu</span>
+                        {/* <span class="fa fa-bars" aria-hidden="true"></span> */}
+                    </a>
+                    
+                    {/* <h1 class="logo">hamburgers</h1> */}
+                    
+                    <nav id="main-menu" class="main-menu" aria-label="Main menu">
+                        <a href="#main-menu-toggle"
+                        id="main-menu-close"
+                        class="menu-close"
+                        aria-label="Close main menu">
+                        <span class="sr-only">Close main menu</span>
+                        <FontAwesomeIcon icon={faTimes} />
+                        </a>
+                        <ul>
+                        <li><a href="#greeting-div">Home</a></li>
+                        <li><a href="#about-div">About</a></li>
+                        <li><a href="#skills-div">Skills</a></li>
+                        <li><a href="#portfolio-div">Portfolio</a></li>
+                        <li><a href="#blog-div">Blog</a></li>
+                        <li><a href="#contact-div">Contact</a></li>
                         </ul>
-                    </div>
-                </nav>
+                    </nav>
+                    <a href="#main-menu-toggle"
+                        class="backdrop"
+                        tabindex="-1"
+                        aria-hidden="true" hidden></a>
+                </header>
+                // <nav role='navigation'>
+                //     <div id='menu-toggle'>
+                //         <input type='checkbox'></input>
+                //         <span></span>
+                //         <span></span>
+                //         <span></span>
+                //         <ul id='menu'>
+                //             <a href='#home-div'>Home</a>
+                //             <a href='#about-div'>About</a>
+                //             <a href='#skills-div'>Skills</a>
+                //             <a href='#portfolio-div'>Portfolio</a>
+                //             <a href='#blog-div'>Blog</a>
+                //             <a href='#contact-div'>Contact</a>
+                //         </ul>
+                //     </div>
+                // </nav>
             )
         }
     }
