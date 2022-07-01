dc1a12da23d264bb80c799ced25f001d0a89b584
David Polcari <dpolcari18@gmail.com>
2022-06-30 20:51:12 -0500

    Updated resume, skills and fixed font in form inputs

--- src/containers/Skills.js
+++ src/containers/Skills.js
@@ -4,15 +4,15 @@ import React from 'react'
 import { ReactComponent as Ruby } from '../icons/ruby.svg'
 import { ReactComponent as RubyOnRails } from '../icons/rubyonrails.svg'
 import { ReactComponent as JavaScript } from '../icons/javascript.svg'
-import { ReactComponent as Html } from '../icons/html5.svg'
-import { ReactComponent as Css } from '../icons/css3.svg'
 import { ReactComponent as ReactIcon } from '../icons/react.svg'
 import { ReactComponent as Redux } from '../icons/redux.svg'
-import { ReactComponent as PowerBI } from '../icons/powerbi.svg'
+import { ReactComponent as DataDog } from '../icons/datadog.svg'
 import { ReactComponent as Heroku } from '../icons/heroku.svg'
 import { ReactComponent as Netlify } from '../icons/netlify.svg'
+import { ReactComponent as MySql } from '../icons/mysql.svg'
 import { ReactComponent as Sqlite } from '../icons/sqlite.svg'
 import { ReactComponent as Postgresql } from '../icons/postgresql.svg'
+import { ReactComponent as CircleCI } from '../icons/circleci.svg'
 
 // styling
 import './Skills.css'
@@ -21,13 +21,13 @@ const Skills = () => {
     return(
         <div className='grid-container vertical' id='my-skills'>
             <JavaScript className='icon' alt={'javascript logo'} title={'JavaScript'} fill={'#F7DF1E'} />
-            <Ruby className='icon' alt={'ruby logo'} title={'Ruby'} fill={'#CC342D'}/>
-            <Html className='icon' alt={'html5 logo'} title={'HTML'} fill={'#E34F26'} />
-            <Css className='icon' alt={'css3 logo'} title={'CSS'} fill={'#1572B6'} />
-            <RubyOnRails className='icon' alt={'rails logo'} title={'Rails'} fill={'#CC0000'} />
             <ReactIcon className='icon' alt={'react logo'} title={'React'} fill={'#61DAFB'} />
+            <Ruby className='icon' alt={'ruby logo'} title={'Ruby'} fill={'#CC342D'}/>
+            <RubyOnRails className='icon' alt={'rails logo'} title={'Rails'} fill={'#CC0000'} />
+            <MySql className='icon' alt={'mysql logo'} title={'MySQL'} />
             <Redux className='icon' alt={'redux logo'} title={'Redux'} fill={'#764ABC'} />
-            <PowerBI className='icon' alt={'powerbi logo'} title={'PowerBI'} fill={'#F2C811'} />
+            <DataDog className='icon' alt={'datadog logo'} title={'DataDog'} />
+            <CircleCI className='icon' alt={'circleci logo'} title={'CircleCI'} />
             <Heroku className='icon' alt={'heroku logo'} title={'Heroku'} fill={'#430098'} />
             <Netlify className='icon' alt={'netlify logo'} title={'Netlify'} fill={'#00C7B7'} />
             <Sqlite className='icon' alt={'sqlite logo'} title={'SQLite'} fill={'#003B57'} />
